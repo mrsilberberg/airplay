667be3239206f4ebeea2b4681a7f6e3fa3af8473
mrsilberberg <161399872+mrsilberberg@users.noreply.github.com>
2025-07-30 18:38:18 -0300

    Update run.sh

--- linux_theme_player_addon/run.sh
+++ linux_theme_player_addon/run.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bash
+
 echo "🔊 Tocando tema de inicialização..."
-mpg123 /theme.mp3
+
+# Toca o arquivo mp3 usando alsa no dispositivo correto
+mpg123 -o alsa -a plughw:1,0 /theme/music.mp3
